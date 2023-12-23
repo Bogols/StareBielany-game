d994c52cee988696dcb7da5faddca947ccacb1eb
Jakub Wilk <jwilk436@gmail.com>
2023-12-23 23:31:54 +0100

    refactor: update map loading and texture atlas creation

--- src/setup/mod.rs
+++ src/setup/mod.rs
@@ -28,17 +28,10 @@ pub fn setup(
         Player {},
     ));
 
-    let map = load_map_from_json("assets/test12.json");
+    let map = load_map_from_json("assets/test15.json");
 
     let texture_handle = asset_server.load("metro-tiles.png");
-    let texture_atlas = create_texture_atlas(
-        texture_handle,
-        &mut texture_atlases,
-        map.tilewidth,
-        map.tileheight,
-        map.width,
-        map.height,
-    );
+    let texture_atlas = create_texture_atlas(texture_handle, &mut texture_atlases, &map);
 
     spawn_tiles(&mut commands, &map, &texture_atlas);
 }
@@ -46,28 +39,30 @@ pub fn setup(
 fn create_texture_atlas(
     texture_handle: Handle<Image>,
     texture_atlases: &mut ResMut<Assets<TextureAtlas>>,
-    tile_width: u32,
-    tile_height: u32,
-    map_width: u32,
-    map_height: u32,
+    map: &Map,
 ) -> Handle<TextureAtlas> {
-    let tile_size = tile_width as f32;
-    let tilemap_width = map_width * tile_width;
-    let tilemap_height = map_height * tile_height;
+    let tileset = &map.tilesets[0];
+    let tile_width = tileset.tilewidth;
+    let tile_height = tileset.tileheight;
+    let image_width = tileset.imagewidth;
+    let image_height = tileset.imageheight;
+
+    let cols = image_width / tile_width;
+    let rows = image_height / tile_height;
 
     let mut texture_atlas = TextureAtlas::new_empty(
         texture_handle,
-        Vec2::new(tilemap_width as f32, tilemap_height as f32),
+        Vec2::new(image_width as f32, image_height as f32),
     );
 
-    let cols = map_width;
-    let rows = map_height;
-
-    for y in 0..rows as i32 {
-        for x in 0..cols as i32 {
+    for y in 0..rows {
+        for x in 0..cols {
             let tile = Rect {
-                min: Vec2::new(x as f32 * tile_size, y as f32 * tile_size),
-                max: Vec2::new((x + 1) as f32 * tile_size, (y + 1) as f32 * tile_size),
+                min: Vec2::new(x as f32 * tile_width as f32, y as f32 * tile_height as f32),
+                max: Vec2::new(
+                    (x + 1) as f32 * tile_width as f32,
+                    (y + 1) as f32 * tile_height as f32,
+                ),
             };
             texture_atlas.add_texture(tile);
         }
@@ -77,32 +72,26 @@ fn create_texture_atlas(
 }
 
 fn spawn_tiles(commands: &mut Commands, map: &Map, texture_atlas_handle: &Handle<TextureAtlas>) {
-    let mut z_index = 0.0;
-    let tile_size = map.tilewidth as f32;
+    let tile_width = map.tilewidth;
+    let tile_height = map.tileheight;
     let tiles_in_row = map.width as usize;
+    let tiles_in_col = map.height as usize;
 
-    println!("tiles_in_row: {}", tiles_in_row);
-
-    for layer in &map.layers {
-        for (index, &tile_id) in layer.data.iter().enumerate() {
-            if tile_id == 0 {
-                continue;
-            }
-
-            let tile_texture_index = tile_id - 1;
-
-            let x_pos = (index % tiles_in_row) as f32 * tile_size;
-            let y_pos = (index / tiles_in_row) as f32 * tile_size;
-
-            println!("x_pos: {}, y_pos: {}", x_pos, y_pos);
-
-            commands.spawn(SpriteSheetBundle {
-                sprite: TextureAtlasSprite::new(tile_texture_index as usize),
-                texture_atlas: texture_atlas_handle.clone(),
-                transform: Transform::from_xyz(x_pos, y_pos, z_index),
-                ..Default::default()
-            });
+    for (index, &tile_id) in map.layers[0].data.iter().enumerate() {
+        if tile_id == 0 {
+            continue;
         }
-        z_index += 1.0;
+
+        let atlas_index = (tile_id - 1) as usize % (tiles_in_row * tiles_in_row);
+
+        let x_pos = (index % tiles_in_row) as f32 * tile_width as f32;
+        let y_pos = ((tiles_in_col - 1) - (index / tiles_in_row)) as f32 * tile_height as f32;
+
+        commands.spawn(SpriteSheetBundle {
+            sprite: TextureAtlasSprite::new(atlas_index),
+            texture_atlas: texture_atlas_handle.clone(),
+            transform: Transform::from_xyz(x_pos, y_pos, 0.0),
+            ..Default::default()
+        });
     }
 }
