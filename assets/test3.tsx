612fa64ba9a85ff645f56aa8c747c633f165e054
Jakub Wilk <jwilk436@gmail.com>
2023-12-23 23:31:54 +0100

    fix: remove unnecessary XML tileset definitions

--- assets/test3.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="test3" tilewidth="32" tileheight="16" tilecount="12" columns="2">
- <image source="metro-tiles.png" width="64" height="96"/>
-</tileset>
