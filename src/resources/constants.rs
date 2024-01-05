7e215a50b0eb0070fdca19d424310969593f5a69
Jakub Wilk <jwilk436@gmail.com>
2024-01-06 00:31:42 +0100

    refactor: update player speed constant and remove unused code

--- src/resources/constants.rs
+++ src/resources/constants.rs
@@ -1,6 +1,1 @@
 pub const PLAYER_SPEED: f32 = 200.0;
-pub const PLAYER_SIZE: f32 = 64.0; // This is the player sprite size.
-pub const HALF_PLAYER_SIZE: f32 = PLAYER_SIZE / 2.00;
-
-pub const TEXTURE_PATH: &str = "map-spritesheet.png";
-pub const TILE_MAP_PATH: &str = "assets/sandbox.json";
