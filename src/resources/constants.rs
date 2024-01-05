b62384c97f7c78cc57bcb6c3d0f82799ba451da9
zer0xday <pawel.majcherczyk.it@gmail.com>
2024-01-05 01:32:19 +0100

    feat: prototype physics system

--- src/resources/constants.rs
+++ src/resources/constants.rs
@@ -1,1 +1,6 @@
-pub const PLAYER_SPEED: f32 = 200.0;
+pub const PLAYER_SPEED: f32 = 4000.0;
+pub const PLAYER_SIZE: f32 = 64.0; // This is the player sprite size.
+pub const HALF_PLAYER_SIZE: f32 = PLAYER_SIZE / 2.00;
+
+pub const TEXTURE_PATH: &str = "metro-tiles.png";
+pub const TILE_MAP_PATH: &str = "assets/test15.json";
